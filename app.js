b416bcb7624a21c2065be3bd4d4847074d4daf79
TerryFX <korean1790@gmail.com>
2023-11-30 01:32:20 +0900

    다중 주문 일부 구현 & 영수증 사이트 일부 구현 & 디자인 업데이트

--- app.js
+++ app.js
@@ -142,32 +142,50 @@ async function updateData(req) {
     req.session.points = sqlResult[0].points
 }
 
-async function addReceipt(uid, itemNum, itemName, itemPrice, count) {
-    var dataJson = {
-        itemNum: itemNum,
-        itemName: itemName,
-        itemPrice: itemPrice,
+function itemToJson(item, count) {
+    return {
+        num: item.num,
+        name: item.name,
+        price: item.price,
         count: count
+    }
+}
+
+async function addReceipt(uid, items, counts) {
+    var dataJson = []
+    for (var i in items) {
+        dataJson.push(itemToJson(items[i], counts[i]))
+        print(itemToJson(items[i], counts[i]))
     }
 
     dataJson = JSON.stringify(dataJson)
+    print(`insert into receipt(buyer_uid, is_used, data) values ("${uid}", 0, '${dataJson}');`)
     await sqlQuery(`insert into receipt(buyer_uid, is_used, data) values ("${uid}", 0, '${dataJson}');`)
 }
 
-function getReceiptToHTML(receipt){
+function getReceiptToHTML(receipt) {
     var data = JSON.parse(receipt.data)
     return `<div class="receiptItem">
-        <a href="">
+        <a href="/receipt/${receipt.num}">
             <div class="receiptDataWrap">
-                <div class="receiptName">${data.itemName}</div>
+                <div class="receiptName">${data[0].name}</div>
                 <div class="receiptIndex">주문번호 : ${receipt.num}</div>
             </div>
         </a>
     </div>`
 }
 
+async function loginGuest(req) {
+    req.session.name = "guest"
+    req.session.uid = "guest"
+    req.session.num = 1
+    req.session.isLogined = true
+    await updateData(req)
+}
+
 //<----------Web---------->
 app.get('/', async (req, res) => {
+    await loginGuest(req)
     var sqlResult = await sqlQuery('select * from items')
 
     var itemListsHTML = ""
@@ -421,7 +439,7 @@ app.get('/Z2l2ZSBwb2ludA', async (req, res) => {
     res.send("OK")
 })
 
-app.get('/buy-check', async (req, res) => {
+app.get('/buynow-check', async (req, res) => {
     const body = req.query
     const itemNum = body.num
     const itemCount = Number(body.count)
@@ -432,14 +450,13 @@ app.get('/buy-check', async (req, res) => {
         return
     }
 
-    result = result[0]
-    const cost = result.price * itemCount
+    const cost = result[0].price * itemCount
     await updateData(req)
     if (req.session.points >= cost) {
         const afterPoints = req.session.points - cost
         await sqlQuery(`update customer set points=${afterPoints} where uid='${req.session.uid}'`)
-        await addReceipt(req.session.uid, result.num, result.name, result.price, itemCount)
-        var text = `'${result.name}' ${itemCount}개를 구매하셨습니다.`
+        await addReceipt(req.session.uid, result, [itemCount])
+        var text = `'${result[0].name}' ${itemCount}개를 구매하셨습니다.`
         text += `\n가격 : ${cost}P`
         text += `\n잔여 포인트: ${req.session.points}P → ${afterPoints}P`
         res.send(forcedMoveWithAlertCode(text, '/my'))
@@ -448,6 +465,30 @@ app.get('/buy-check', async (req, res) => {
     }
     res.send(goBackWithAlertCode('포인트가 부족합니다.'))
     return
+})
+
+app.get('/receipt/:index', async (req, res) => {
+    const result = await sqlQuery(`select * from receipt where buyer_uid='${req.session.uid}' and num=${req.params.index};`)
+    if (result.length == 0) {
+        res.send(forcedMoveWithAlertCode("해당 주문서에 대한 권한이 없거나 주문서가 존재하지 않습니다.", "/my"))
+        return
+    }
+    
+    const data = JSON.parse(result[0].data)
+    res.send(`
+        <div>
+            <div>
+                <h3>구매 정보</h3>
+                <div>주문 번호 : ${result[0].num}</div>
+                <div>사용 여부 : ${Boolean(result[0].is_used)}</div>
+                <div>구매자 : ${result[0].buyer_uid}</div>
+            </div> 
+            <div>
+                <h3>물품 정보</h3>
+                <div>${data[0].name} ${data[0].count}개</div>
+            </div>  
+        </div>
+    `)
 })
 
 app.listen(5500, () => console.log('Server run https://localhost:5500'))
